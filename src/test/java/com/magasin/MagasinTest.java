3ad292841de22deadd74db03058e1ac12a778a91
Anne-Laure-PLANO <plano-devillard@orange.fr>
2026-03-24 14:22:49 +0100

    Création d'un Golden Master : liste pour tests récupérant l'ensemble des possibilités de création d'items.

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -1,15 +1,27 @@
 package com.magasin;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
 
-    private void listeATester(){
-        
-    }
 
+// création d'un golden Master : ensemble de cas possibles à tester
+    @BeforeEach
+    void setUp(){
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Comté",10,10));
+        items.add(new Item("Normal", 30, 50));
+        items.add(new Item("Kryptonite", 100, 80));
+        items.add(new Item("Pass VIP Concert", 20, 25));
+        items.add(new Item("Pouvoirs magiques", 50, 50));
+    }
 
 
     @Test
