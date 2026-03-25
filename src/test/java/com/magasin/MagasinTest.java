3ae7d618a4c77570871a2d12ccfa277badb1d93e
Anne-Laure-PLANO <plano-devillard@orange.fr>
2026-03-25 09:38:14 +0100

    Reprise du golden Master : retravail du principe : le golden master instancie des objets et enregistre leur comportement, afin qu'on puisse vérifier, après travail sur le projet, que ce dernier n'a pas été altéré. - Création d'une classe GoldenMasterTest.java qui créé un tableau d'objets contenant des valeurs tests clés à surveiller.

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -1,6 +1,5 @@
 package com.magasin;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,24 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
 
-// création d'un golden Master : ensemble de cas possibles à tester
-    @BeforeEach
-    void setUp(){
-        List<Item> items = new ArrayList<>();
-        items.add(new Item("Comté",10,10));
-        items.add(new Item("Normal", 30, 50));
-        items.add(new Item("Kryptonite", 100, 80));
-        items.add(new Item("Pass VIP Concert", 20, 25));
-        items.add(new Item("Pouvoirs magiques", 50, 50));
-    }
-
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
 }
