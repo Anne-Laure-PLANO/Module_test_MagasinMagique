f620598cfaf1246a7c6584d23b7e92ab983471fa
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-03-25 17:40:05 +0100

    test unitaire kriptonite

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -10,8 +10,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
 
+@Test
+void Kryptonite_TestDatePeremption_PasDeChangement() {
+    Item[] items = new Item[]{
+            new Item("Kryptonite", 0, 80)
+    };
+    Magasin app = new Magasin(items);
 
+    app.updateQuality();
 
+    assertEquals(0, items[0].sellIn);
+    assertEquals(80, items[0].quality);
+}
     @Test
     void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
@@ -19,5 +29,6 @@ class MagasinTest {
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
+
 
 }
