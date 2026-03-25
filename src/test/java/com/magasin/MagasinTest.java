422c98432295c12b6904a88d0d9b9335a98057c8
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-03-25 10:22:58 +0100

    testgoldenMaster

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -1,36 +1,44 @@
 package com.magasin;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
 
+    @Test
+    void testGoldenMaster() {
+        Item[] items = new Item[]{
+                new Item("Comté", 10, 10),
+                new Item("Normal", 30, 50),
+                new Item("Kryptonite", 100, 80),
+                new Item("Pass VIP Concert", 20, 25),
+                new Item("Pouvoirs magiques", 50, 50)
+        };
 
+        Magasin app = new Magasin(items);
 
-// création d'un golden Master : ensemble de cas possibles à tester
-    @BeforeEach
-    void setUp(){
-        List<Item> items = new ArrayList<>();
-        items.add(new Item("Comté",10,10));
-        items.add(new Item("Normal", 30, 50));
-        items.add(new Item("Kryptonite", 100, 80));
-        items.add(new Item("Pass VIP Concert", 20, 25));
-        items.add(new Item("Pouvoirs magiques", 50, 50));
+        StringBuilder result = new StringBuilder();
+
+        for (int day = 0; day < 10; day++) {
+            result.append(Arrays.toString(items)).append("\n");
+            app.updateQuality();
+            System.out.println(result.toString());
+        }
+
     }
-
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
-
 }
+
+
